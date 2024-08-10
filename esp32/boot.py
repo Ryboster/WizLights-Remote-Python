0bf10546e403617fa800c52a56d53b61abc3cb30
Ryboster <23017998@uhi.ac.uk>
2024-08-10 07:08:19 +0100

    added dynamic light address seeking

--- esp32/boot.py
+++ esp32/boot.py
@@ -22,14 +22,14 @@ SSID = 'IoT21'
 PASSWORD = 'EstIstEinPassword'
 PORT = 38899
 SUBNET = "192.168"
+SUBNET_MASK = "192.168.0"
+
 
 class Kek():
     def __init__(self):
 
-        self.LIGHT_IP = "192.168.0.3"
-        # Make your light's IP permanent with 
-        # echo -n "{\"id\":1,\"method\":\"setPilot\",\"params\":{\"static_ip\":\"192.168.0.5\",\"gateway\":\"192.168.0.1\",\"mask\":\"255.255.255.0\",\"dns1\":\"8.8.8.8\",\"dns2\":\"8.8.4.4\"}}"
-        # | nc -u -w 1 192.168.XX.XX 38899
+        self.LIGHT_IP = None
+
 
 
         self.ap = network.WLAN(network.AP_IF)
@@ -49,9 +49,29 @@ class Kek():
         print('SSID:', SSID)
         print('IP address:', self.ap.ifconfig()[0])
 
+
+    def scan_for_light(self):
+        message = b'{"method":"getPilot","params":{}}'
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        
+        while not self.LIGHT_IP:
+            for i in range(0, 254):
+                addr = (SUBNET_MASK + f".{i}", PORT)
+                print("scanning", addr)
+                sock.sendto(message, addr)
+                sock.settimeout(0.5)
+                try:
+                    data, addr = sock.recvfrom(1024)
+                    print(addr)
+                    self.LIGHT_IP = str(addr[0])
+                    break
+                except OSError:
+                    self.LIGHT_IP = None
+
     
-    def send_udp_message(self, ip, port, message):
+    def send_udp_message(self, ip, port, message):   
         addr = (ip, port)
+        print("sending message to: ", addr)
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.sendto(message, addr) 
         s.close()
@@ -81,6 +101,9 @@ class Kek():
             print("Dimming light")
             message = '{"id":1,"method":"setPilot","params":{"temp":4166,"dimming":10}}'
             self.send_udp_message(self.LIGHT_IP, PORT, message)
+
+        if formatted_data == '47':
+            self.scan_for_light()
 
 
         
